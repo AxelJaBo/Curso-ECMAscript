b97eb34de12e9af297754c56f17878efef80bb76
Daniel <axel.910921@gmail.com>
2023-05-02 19:19:30 -0700

    ES6: strings

--- src/es6/01-arrow-function.js
+++ src/es6/01-arrow-function.js
@@ -7,3 +7,13 @@ const square = (num) =>{
 }
 
 const square = num => num*num;
+
+// Multi-line strings
+
+let lorem = 'esto es un string\n' + 'esto es otra linea';
+
+let lorem2 = `Esta es una frase epica
+    la continuación de esa frase epica.
+`;
+console.log(lorem);
+console.log(lorem2);
